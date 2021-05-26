85cd1d54968e736ca7e7e05dbe7b638f6338373c
Yassier Gania <yassiergania09@gmail.com>
2021-05-26 13:38:27 +0800

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout>
+    <q-header elevated class="bg-indigo-10">
+      <q-toolbar>
+        <q-btn icon="menu" flat round size="sm"></q-btn>
+        <q-toolbar-title>SMS CASTER</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
