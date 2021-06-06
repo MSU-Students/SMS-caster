c764226709eda1f355a7152a4132200865eb69ea
mosaabtalib <mosaab.talib09@gmail.com>
2021-06-06 13:03:26 +0800

    add table at manage contact

--- src/pages/ManageContact.vue
+++ src/pages/ManageContact.vue
@@ -1,5 +1,14 @@
 <template>
-    <q-page class="bg-grey-12">
-        Manage Contact is here
-    </q-page>
+  <q-page class="bg-grey-12">
+    <div class="q-pa-md">
+      <q-table
+        title="Contacts"
+        :data="data"
+        :columns="columns"
+        row-key="name"
+        dark
+        color="amber"
+      />
+    </div>
+  </q-page>
 </template>
